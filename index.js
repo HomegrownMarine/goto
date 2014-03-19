6f8a631127293d9cb3e8b3d95ce963370583940d
Grady Morgan <gradymorgan@gmail.com>
2014-03-18 21:40:07 -0700

    add mostly working goto app, still need to tie in with data and settings services

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var express = require('express');
 var server = express();
 server.use(express.urlencoded());
 
+server.get('/lib/jquery.js', function(req, res){
+    res.redirect('http://code.jquery.com/jquery-2.1.0.min.js');
+});
+server.get('/lib/handlebars.js', function(req, res){
+    res.redirect('http://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-v1.3.0.js');
+});
+
 // serve randomly changing data
 server.get('/now', function(req, res){
     res.send(JSON.stringify({}));
