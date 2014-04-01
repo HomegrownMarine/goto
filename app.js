2d94d8bd96f9bb70dcf292231a2a95b9f65c6d65
Grady Morgan <gradymorgan@gmail.com>
2014-03-31 22:08:06 -0700

    add navigation calcs

--- app.js
+++ app.js
@@ -8,7 +8,65 @@ var current = null;
 var history = null;
 var settings = null;
 
-//TODO: calculate navigation data, and repost
+//TODO: move calcs to util and add unit tests
+var R = '3440.06479'; //radius of earth in nautical miles
+
+function deg(radians) {
+    return (radians*180/Math.PI + 360) % 360;
+}    
+
+function rad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+//see: http://www.movable-type.co.uk/scripts/latlong.html
+function distance(lat1, lon1, lat2, lon2) {
+    lat1 = rad(lat1);
+    lat2 = rad(lat2);
+    lon1 = rad(lon1);
+    lon2 = rad(lon2);
+
+    var dLat = lat2-lat1,
+        dLon = lon2-lon1;
+    
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return R * c;
+}
+
+// distance(47.67962,-122.40555,47.67897, -122.3767); --> 1.1668
+
+function bearing(lat1, lon1, lat2, lon2) {
+    lat1 = rad(lat1)
+    lat2 = rad(lat2)
+    lon1 = rad(lon1)
+    lon2 = rad(lon2)
+    
+    var dLon = lon2-lon1;
+    
+    var y = Math.sin(dLon)*Math.cos(lat2);
+    var x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+    
+    return deg( Math.atan2(y, x) );
+}
+
+function steer(hdg1, hdg2) {
+    var diff = Math.abs(hdg1 - hdg2);
+    if ( diff > 180 ) {
+        diff = 360 - diff;
+    }
+    return diff;
+}
+
+function crossTrackError( fromLat, fromLon, lat, lon, toLat, toLan ) {
+    var d = distance(fromLat, fromLon, toLat, toLan);
+    var b1 = bearing(fromLat, fromLon, toLat, toLan);
+    var b2 = bearing(fromLat, fromLon, lat, lon);
+    return Math.asin(Math.sin(d/R) * Math.sin(rad(b2-b1))) * R;
+}
+
+// bearing(47.62455,-122.33107,47.63272,-122.34746) -> 306.49
 
 //parse coordinate string in either decimal degrees or decimal 
 //minutes.  Returns float
@@ -58,6 +116,7 @@ function getHistory() {
 exports.load = function(server, boat_data, settings_comp) {
     settings = settings_comp;
 
+    // get app settings
     history = settings.get('goto:waypoint-history') || [];
     current = settings.get('goto:current-waypoint') || {
             'name': 'B',
@@ -65,42 +124,60 @@ exports.load = function(server, boat_data, settings_comp) {
             'lon': -122.4235674,
         };;
 
-    // server.use('/goto', require('less-middleware')(path.join(__dirname, 'www')));
+    
+    // add in app's static content
     server.use('/goto', express.static(path.join(__dirname, 'www')));
 
+    
+    // add in app's dynamic ajax handlers
     server.get('/goto/history', function(req, res){
-        res.send(JSON.stringify(getHistory()));
+        res.send(getHistory());
     });
 
     server.get('/goto/current', function(req, res){
-        res.send(JSON.stringify(getCurrentWaypoint()));
+        res.send(getCurrentWaypoint());
     });
 
     server.post('/goto/current', function(req, res){
         //add to history and save
 
-        var dat = {
+        var waypoint = {
             'name': req.body.name,
             'lat': coordinate(req.body.lat),
             'lon': coordinate(req.body.lon),
         };
 
-        setCurrentWaypoint(dat);
-        res.send(JSON.stringify(current));
+        setCurrentWaypoint(waypoint);
+        res.send(current);
     });
 
-    boat_data.on('data:rmc', function(msg) {
-        var rmb = {
-            type: 'rmb'
-
-        };
-
-        // TODO: calculate waypoiny stuff and broadcast it
-
-        boat_data.broadcast(null, rmb);
+    //TODO: allow no waypoint.
+    boat_data.on('data:rmc', function(data) {
+        if ( current === null ) return;
+        try {
+            var dtw = distance( data['lat'], data['lon'], current.lat, current.lon );
+            var btw = bearing( data['lat'], data['lon'], current.lat, current.lon );
+            var vmg = data['sog'] * Math.cos(rad(btw - data['cog']));
+            
+            //var xte = crossTrackError( fromWP.latitude, fromWP.longitude, data['lat'], data['lon'], destinationWP.latitude, destinationWP.longitude )
+            var rmb = {
+                type: 'rmb',
+                to: current,
+                from: {name:'X'}, //TODO:
+                dtw: dtw,
+                btw: btw,
+                vmgw: vmg,
+                dts: steer(btw, data.cog)>=0?'L':'R', //TODO:
+                xte: 0
+            };
+            boat_data.broadcast(null, rmb);
+        }
+        catch(e) {
+            console.error('error calculating nav', data, current);
+        }
     });
 
-    return ['/goto', 'GoTo Waypoint'];
+    return {url:'/goto/', title:'GoTo Waypoint', priority: 1};
 };
 
 
